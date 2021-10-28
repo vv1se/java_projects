9b49a1c0d749408b1397e2dda25d9725f8a44a3a
vv1se <uniq.rantopad@mail.ru>
2021-10-28 16:54:49 +0300

    изменены отступы

--- sandbox/src/main/java/Andrew/jft/sanbox/MyFirstProgram.java
+++ sandbox/src/main/java/Andrew/jft/sanbox/MyFirstProgram.java
@@ -3,8 +3,8 @@ package Andrew.jft.sanbox;
 public class MyFirstProgram {
 
 
-	public static void main(String[] args) {
-		System.out.println("Hello, world!");
+  public static void main(String[] args) {
+    System.out.println("Hello, world!");
 
-	}
+  }
 }
